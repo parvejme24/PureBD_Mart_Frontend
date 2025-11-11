621147ccf8416098ca463e0fe7848bf2fabf6f9f
Md Parvej <mdparvejme24@gmail.com>
2025-11-12 02:20:57 +0600

    added testimonial section

--- app/(WithCommonLayout)/(home)/page.jsx
+++ app/(WithCommonLayout)/(home)/page.jsx
@@ -2,6 +2,7 @@ import Banner from "@/components/module/HomePageComp/Banner/Banner";
 import Category from "@/components/module/HomePageComp/Category/Category";
 import DealOfTheDay from "@/components/module/HomePageComp/DealOfTheDay/DealOfTheDay";
 import Product from "@/components/module/HomePageComp/Product/Product";
+import Testimonial from "@/components/module/HomePageComp/Testimonial/Testimonial";
 import React from "react";
 
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Category />
       <Product />
       <DealOfTheDay />
+      <Testimonial />
     </div>
   );
 }
---
+++ components/module/HomePageComp/Testimonial/Testimonial.jsx
@@ -0,0 +1,38 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function Testimonial() {
+  return (
+    <div
+      className="container mx-5 md:mx-auto max-w-7xl rounded-xl my-10"
+      style={{
+        backgroundImage:
+          'url("https://themepanthers.com/wp/nest/d1/wp-content/uploads/2022/02/banner-10-min.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: "80px 50px",
+      }}
+    >
+      <div className="max-w-lg space-y-4 text-left">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-800">
+          Stay home & get your daily needs from our shop
+        </h2>
+        <p className="text-gray-600 text-sm md:text-base">
+          Start your daily shopping with{" "}
+          <span className="text-blue-600 font-semibold">PureBD Mart</span>
+        </p>
+
+        <div className="flex flex-col sm:flex-row mt-4 space-y-1">
+          <Input
+            placeholder="Enter email address"
+            className="rounded-full px-5 py-5 bg-white/90 border-gray-300"
+          />
+          <Button className="md:-ml-[113px] cursor-pointer py-4 mt-[3px] rounded-full bg-blue-600 hover:bg-blue-700 text-white px-6">
+            Subscribe
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
